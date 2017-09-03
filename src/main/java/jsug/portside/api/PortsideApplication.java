f528071427519e1456c86fe1efb7d96400ee5ae6
making <makingx@gmail.com>
2017-09-03 18:00:08 +0900

    Support Eureka Service Discovery

--- src/main/java/jsug/portside/api/PortsideApplication.java
+++ src/main/java/jsug/portside/api/PortsideApplication.java
@@ -2,10 +2,12 @@ package jsug.portside.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PortsideApplication {
 
 	public static void main(String[] args) throws Exception {
