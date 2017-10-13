f5058929a9e78a45526b141b533c60df236c8728
Kouhei Toki <kouhei.toki@gmail.com>
2017-10-13 11:00:48 +0900

    うっかりダミーデータを本番に登録しないよに、明示的にプロファイルを指定するようにした

--- src/main/java/jsug/portside/api/PortsideApplication.java
+++ src/main/java/jsug/portside/api/PortsideApplication.java
@@ -1,5 +1,6 @@
 package jsug.portside.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,11 +16,24 @@ import org.springframework.context.annotation.Bean;
 public class PortsideApplication {
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ctx  = SpringApplication.run(PortsideApplication.class, args);
-		ctx.getBean(DummyDataCreator.class).createDummyData();
+		SpringApplication.run(PortsideApplication.class, args);
 	}
 	
+	
 	@Bean
+	@Profile("needDummyData")
+	public CommandLineRunner dummyDataCreate(DummyDataCreator dummyDataCreator) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				dummyDataCreator.createDummyData();
+			}
+		};
+	}
+	
+	
+	@Bean
+	@Profile("needDummyData")
 	public DummyDataCreator dummyDataCreator() {
 		return new DummyDataCreator();
 	}
