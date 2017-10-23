01ce8da377c96c2b40a76fcd29633b18cccfc53e
making <makingx@gmail.com>
2017-10-23 13:55:08 +0900

    Allow /cloudfoundryapplication/**

--- src/main/java/jsug/portside/api/config/SecurityConfig.java
+++ src/main/java/jsug/portside/api/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests() //
+				.mvcMatchers("/cloudfoundryapplication/**").permitAll() //
 				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll() //
 				.mvcMatchers(HttpMethod.GET, "/sessions").permitAll() //
 				.mvcMatchers(HttpMethod.GET, "/sessions/withAttendeeCount")
